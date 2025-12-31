5b7ed2a3c5363dcf54d2034b6b1031140d77f3cc
vijaywassan <mvijay@wassan.org>
2025-12-31 19:04:24 +0530

    added logos and footer

--- app.py
+++ app.py
@@ -60,11 +60,13 @@ st.markdown("""
 page1, page2 = st.columns([1, 4])
 with page1:
     st.markdown(" ")
-    st.image("./media/wassan logo.jpg", width=70)
+    st.image("./media/wassan_logo.png", width=70)
 #  # adjust width as needed # Optional: add a subtitle below
 with page2:
     st.markdown(" ")
-    st.markdown("<h2 style='color:#1f77b4;'>Wassan Policy Assistant</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='color:#1f77b4;'>Wassan Internal Policy Assistant</h2>", unsafe_allow_html=True)
+    # st.title("✨ WASSAN Policy Assistant")
+    st.caption("Your interactive guide to organizational HR & other policies, leave rules, benefits and procedures.")
 
     col1, col2 = st.columns([4, 1])
     with col1:
@@ -99,15 +101,56 @@ with page2:
                 index, llm = load_index_from_db()
                 query_engine = index.as_query_engine(llm=llm, similarity_top_k=8)
                 res = query_engine.query(task)
-                response_text = st.markdown(res.response)
-                
 
-                # placeholder = st.empty()
-                # streamed = ""
-                # for word in response_text.split():
-                #     streamed += word + " "
-                #     placeholder.markdown(f"**Assistant:** {streamed}")
-                #     time.sleep(0.05)
+                if not res.response.strip():
+                    st.warning("I couldn’t find a clear answer. \
+                                For further information, please reach out to hr@wassan.org")
+                else:
+                    st.markdown(res.response)
+                    st.markdown("#### For the correctness of the information, Please check with the HR team.")
+                    st.markdown(" ")
+                    st.markdown(" ")
+                    st.markdown(" ")
+                
         except Exception as e:
             st.error(f"❌ Query failed: {e}")
 
+
+
+
+import base64
+
+
+def image_to_base64(path):
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+logo_b64 = image_to_base64("media/logo v2.png")
+
+st.markdown(
+    f"""
+    <style>
+        .footer {{
+            position: fixed;
+            right: 0;              /* 👈 align to right */
+            bottom: 0;             /* 👈 stick to bottom */
+            width:100%;
+            background-color: #f0f8ff;
+            text-align: right;     /* 👈 text aligned right */
+            padding: 10px;
+            font-size: 14px;
+            color: grey;
+            # border-top-left-radius: 8px; /* rounded corner look */
+        }}
+        # .footer img {{
+        #     display: block;
+        #     margin-left: auto;     /* 👈 push image to right */
+        # }}
+    </style>
+    <div class="footer">
+        <img src="data:image/png;base64,{logo_b64}" width="70">
+        Developed by <b>Data Roots Team</b>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
